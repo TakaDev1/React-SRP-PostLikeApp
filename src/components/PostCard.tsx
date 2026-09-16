f259b464404b6a5d6fe7eb8b79cdb40973d71d4b
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-16 17:27:04 +0900

    feat: Change the "Like" button to an image.

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -8,13 +8,21 @@ type PostCardProps = {
 
 const PostCard = ({ post, likeCount, onLike }: PostCardProps) => {
   return (
-    <div className="rounded-lg border p-4 shadow-md">
+    <div className="mx-auto mb-10 w-1/2 rounded-lg border bg-blue-400 p-4 shadow-md">
       <h2 className="text-xl font-bold">{post.title}</h2>
 
       <p className="mt-2 text-gray-700">{post.content}</p>
 
-      <button onClick={onLike} className="mt-4 rounded-md bg-pink-500 px-3 py-1 text-white">
-        いいね ({likeCount})
+      <button
+        onClick={onLike}
+        className="group mt-4 flex items-center gap-2 text-2xl"
+        aria-label="いいね"
+      >
+        <span className="cursor-pointer inline-block transition-transform duration-200 group-active:scale-200 text-center">
+          ❤️
+        </span>
+
+        <span className="text-base text-white">{likeCount}</span>
       </button>
     </div>
   );
