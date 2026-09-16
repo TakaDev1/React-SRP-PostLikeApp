b4fc7d371033246d186e818417f3694bf67b581f
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-16 15:38:24 +0900

    Create useLike Custom Hook

---
+++ src/hooks/useLike.ts
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const useLike = () => {
+  const [likes, setLikes] = useState<Record<number, number>>({});
+
+  const handleLike = (id: number) => {
+    setLikes((prev) => ({ ...prev, [id]: (prev[id] || 0) + 1 }));
+  };
+
+  return { likes, handleLike };
+};
